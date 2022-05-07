c1ac0d33130e908d4212d78e4f4117cd549e009b
prototypemvc <ilyasmirnov20060506@gmail.com>
2022-05-07 10:50:55 +0300

    final update
    final update

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {WebView} from "react-native-webview";
+import * as ScreenOrientation from "expo-screen-orientation";
+export default class App extends React.Component {
+    componentDidMount() {
+        ScreenOrientation.unlockAsync();
+    }
+    render() {
+        return <WebView source={{uri: "https://lazermoviesreactnativewebviewapp.netlify.app/"}} startInLoadingState={true} javaScriptEnabled={true} domStorageEnabled={true} allowsFullscreenVideo={true} />;
+    }
+}
